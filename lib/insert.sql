39007b94f352e31f2c5e58e3c50c06b4c0cc48fa
Ivana <ivana_pile@yahoo.com>
2019-12-15 15:32:39 -0500

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,6 @@
 INSERT INTO Series (title, author_id, subgenre_id) VALUES ("Harry Potter", 1, 1);
-INSERT INTO Series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 2, 2);
+
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("A Song of Ice and Fire", 1, 1);
 
 
 INSERT INTO Books (title, year, series_id) VALUES ("Lord of the Rings: The Fellowship of the Ring", 1954, 2);
